2a1074b244f267c82429ebc8b3f9c261ad0db0bc
tmh9x <thomas.hermanu@gmail.com>
2021-04-28 23:29:24 +0200

    Save tasks to localStorage

--- new.js
+++ new.js
@@ -1,6 +1,22 @@
 const formElement = document.querySelector(".form");
 
+function parseJSONFromLocalStorage(key) {
+  const json = localStorage.getItem(key);
+  const data = JSON.parse(json);
+  return data;
+}
+
+function appendToArray(item, array) {
+  return [...array, item];
+}
+
+function stringifyJSONToLocalStorage(key, value) {
+  const json = JSON.stringify(value);
+  localStorage.setItem(key, json);
+}
+
 formElement.onsubmit = function (event) {
+  // Prevent the default form submit behaivor (sending data to a server an reloading page)
   event.preventDefault();
 
   const textInputElement = document.querySelector(".formInput");
@@ -9,12 +25,12 @@ formElement.onsubmit = function (event) {
   );
 
   if (!textInputElement.value) {
-    alert("Please enter a task");
+    alert("Text Input is empty!");
     return;
   }
 
   if (!checkedDateInput) {
-    alert("Please select a date");
+    alert("Radio Button not checked!");
     return;
   }
 
@@ -23,5 +39,45 @@ formElement.onsubmit = function (event) {
     date: checkedDateInput.value,
   };
 
-  console.log(task);
+  const taskList = parseJSONFromLocalStorage("taskList");
+  const newTaskList = appendToArray(task, taskList);
+  stringifyJSONToLocalStorage("taskList", newTaskList);
 };
+
+////////////////////////////////////////////
+// SAME RESULT WITHOUT CREATING FUNCTIONS //
+////////////////////////////////////////////
+
+// const formElement = document.querySelector(".form");
+
+// formElement.onsubmit = function (event) {
+//   event.preventDefault();
+
+//   const textInputElement = document.querySelector(".formInput");
+//   const checkedDateInput = document.querySelector(
+//     ".radioContainer__input:checked"
+//   );
+
+//   if (!textInputElement.value) {
+//     alert("Please enter a task");
+//     return;
+//   }
+
+//   if (!checkedDateInput) {
+//     alert("Please select a date");
+//     return;
+//   }
+
+//   const task = {
+//     name: textInputElement.value,
+//     date: checkedDateInput.value,
+//   };
+
+//   const taskList = JSON.parse(localStorage.getItem("taskList"));
+
+//   taskList.push(task);
+//   console.log(taskList);
+
+//   localStorage.setItem("taskList", JSON.stringify(taskList));
+
+// };
